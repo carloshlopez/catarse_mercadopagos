d57ed99c2421bf9a171e535645ecc8321fdcdc42
Carlos Lopez(mac-mini) <carloshlopez@gmail.com>
2015-06-09 16:05:26 -0500

    try agian

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ CatarseMercadopagos::Engine.routes.draw do
   namespace :payment do
     get '/mercadopagos/:id_contribution/review' => 'mercadopagos#review', :as => 'review_mercadopagos'
     post '/mercadopagos/notifications' => 'mercadopagos#ipn',  :as => 'ipn_mercadopagos'
+    get '/mercadopagos/notifications' => 'mercadopagos#ipn',  :as => 'ipn_mercadopagos_get'
     post '/mercadopagos/:id_contribution/notifications' => 'mercadopagos#notifications',  :as => 'notifications_mercadopagos'
     get '/mercadopagos/:id_conribution/notifications' => 'mercadopagos#notifications',  :as => 'notifications_mercadopagos_get'
     get '/mercadopagos/:id_contribution/success'       => 'mercadopagos#success',        :as => 'success_mercadopagos'
